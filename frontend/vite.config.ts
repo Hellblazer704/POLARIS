6f75d128a921fa6110be84f99c8f1dc6c3c29c7f
Paridhi Sharma <paridhi0203sharma@gmail.com>
2026-03-28 08:54:51 +0530

    Delete frontend directory

--- frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: '/POLARIS/',
-  plugins: [react(), tailwindcss()],
-})
